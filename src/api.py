4acbf838c92312192698e364ef44ecaeada24b5d
Rafa Leyva Ruiz <rafaelleru95103@gmail.com>
2020-01-26 10:55:28 +0100

    update circleci config to not use docker in remote machine. Update api port

--- src/api.py
+++ src/api.py
@@ -40,5 +40,5 @@ if __name__ == '__main__':
         config.scan()
         app = config.make_wsgi_app()
 
-    server = make_server('0.0.0.0', 80, app)
+    server = make_server('0.0.0.0', 8000, app)
     server.serve_forever()
